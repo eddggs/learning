184245f9870d34411f3072b4a723ceef5ccf53b4
eddggs <edy10xxx@gmail.com>
2024-03-04 23:22:14 +0100

    homework

---
+++ lesson 4/lesson4.js
@@ -0,0 +1,26 @@
+const names = [11, 21, 33, 4, 5, 6, 7, 8];
+
+//for( i = 0; i < names.length; i = i + 2){
+//console.log(i);
+//console.log(names[i]);
+
+//};
+
+
+
+//let i = 1;
+//i = i + 1;
+//i++;
+//++i;
+//i += 1;
+
+//console.log(i)
+
+// const num = 13; // dividir
+// const b = num % 2;
+// console.log (b);
+
+
+// const c = [45, 32];
+// c.push (3);// push mete cualquier cosa dentro
+// c.push (100); // mete mas cosas
